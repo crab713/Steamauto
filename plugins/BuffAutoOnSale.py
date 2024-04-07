0b30cfd351610890ea7b9a3d2426493db23b6c61
crab <835314623@qq.com>
2024-04-07 14:22:04 +0800

    发送报价限速

--- plugins/BuffAutoOnSale.py
+++ plugins/BuffAutoOnSale.py
@@ -685,6 +685,7 @@ class BuffAutoOnSale:
             if resp_json["code"] == "OK":
                 self.unfinish_supply_order_list.append({"order_id":order_id, "create_time":time.time()})
                 self.logger.info("[BuffAutoOnSale] 发起steam报价成功! ")
+                time.sleep(10)
             return True
         else:
             self.logger.error(response_json)
